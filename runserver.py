c6b066eacfdac9b1057b06b9f323df23f928f55c
Amirreza Ajam <AmirRetzio@gmail.com>
2025-09-28 12:52:43 +0330

    1.1

--- runserver.py
+++ runserver.py
@@ -8,11 +8,9 @@ def main():
     # Auto-run migrations
     execute_from_command_line([sys.argv[0], 'migrate', '--noinput'])
     # Start server
-    execute_from_command_line([sys.argv[0], 'runserver', '0.0.0.0:8000'])
+    port = os.environ.get('PORT', '8000')
+    execute_from_command_line([sys.argv[0], 'runserver', f'0.0.0.0:{port}'])
 
 
 if __name__ == '__main__':
     main()
-
-
-
