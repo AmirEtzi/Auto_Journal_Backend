ab45a11675f071213bf44e8379dae695eb7d83c4
Amirreza Ajam <AmirRetzio@gmail.com>
2025-09-28 13:28:18 +0330

    1.2

---
+++ create_superuser.py
@@ -0,0 +1,21 @@
+from django.contrib.auth import get_user_model
+import os
+import django
+
+os.environ.setdefault("DJANGO_SETTINGS_MODULE",
+                      "your_project_name.settings")  # اسم پروژه‌ات رو بزار
+django.setup()
+
+
+User = get_user_model()
+
+username = "Etzi"
+password = "2323"
+email = "Etzi2323@gmail.com"
+
+if not User.objects.filter(username=username).exists():
+    User.objects.create_superuser(
+        username=username, email=email, password=password)
+    print(f"Superuser '{username}' created.")
+else:
+    print(f"Superuser '{username}' already exists.")
